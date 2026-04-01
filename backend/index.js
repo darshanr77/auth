ac04accf3e1111b5d18606327ba84b26c3785de6
Darshan R <darshankok7@gmail.com>
2026-04-01 06:01:08 +0530

    Initialize backend structure with user model, authentication routes, and server setup

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("api/auth",authRoutes);
 
 app.get("/", (req, res) => {
   res.send("You are in main page congratulations!");
 });
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth",authRoutes);
 
 
 connectDB();
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,32 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// export const userSchema = mongoose.model{
+export const userSchema = new mongoose.model({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    lastlogin:{
+        type:Date,
+        default:Date.now,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    resetPassowrdToken:String,
+    resetPasswordExpiresAt:Date,
+    verificationToken:String,
+    verificationTokenExpiresAt:Date
+},{timestamps:true});
 
-// }
 
-
-// export const User = mongoose.model("User",userSchema);
+export const User = mongoose.model("User",userSchema);
